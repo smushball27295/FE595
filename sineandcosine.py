13bb272cf5ccdc773c1ea947bf4be8e3678b8600
smushball27295 <51070088+smushball27295@users.noreply.github.com>
2020-02-11 16:25:39 -0500

    Update sineandcosine.py

--- sineandcosine.py
+++ sineandcosine.py
@@ -11,13 +11,9 @@ import matplotlib.pyplot as plt
 def func(): 
     x = np.arange(0,2*np.pi,0.1)   
     y = np.sin(x)
-    x = np.arange(0,2*np.pi,0.1)   
-    y = np.sin(x)
     z = np.cos(x)
     plt.plot(x,y,x,z)
     plt.show()
-    
-func()    
 
 if _name_=="_main_":
     func()
@@ -43,9 +39,3 @@ if _name_=="_main_":
 
 
 
-plt.plot(x,y,x,z)
-plt.xlabel('x values from 0 to 4pi')  # string must be enclosed with quotes '  '
-plt.ylabel('sin(x) and cos(x)')
-plt.title('Plot of sin and cos from 0 to 4pi')
-plt.legend(['sin(x)', 'cos(x)'])      # legend entries as seperate strings in a list
-plt.show()
